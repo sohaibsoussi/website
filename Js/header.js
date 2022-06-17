6a0eb860d93cd0cf8ebc27ebfb39f11c1c03c991
sohaibsoussi <84712377+sohaibsoussi@users.noreply.github.com>
2022-06-17 23:56:39 +0100

    Add files via upload

---
+++ Js/header.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    var menuToggle = document.querySelector(".toggle");
+    var navigation = document.querySelector(".navigation2");
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
